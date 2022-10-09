3034ba2516985ec009d76106c9c3198c6de83f06
Yilfri Salave <yilfri97@hotmail.com>
2022-10-08 22:26:17 -0400

    Incremental Static Regeneration added

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -4,9 +4,13 @@ import { Pokemon } from '../interfaces';
 export const getPokemonInfo = async (nameOrId: string) => {
 	const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-	return {
-		id: data.id,
-		name: data.name,
-		sprites: data.sprites
-	};
+	try {
+		return {
+			id: data.id,
+			name: data.name,
+			sprites: data.sprites
+		};
+	} catch (error) {
+		return null;
+	}
 };
